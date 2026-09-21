5008c836e8df977eaf0d597a783c5c59fc88d146
Sean Huh <sokwhan@google.com>
2026-09-21 15:40:52 -0700

    Bridge legacy TypeProvider into CelTypeProvider to decouple checker internals
    PiperOrigin-RevId: 985555726

--- checker/src/test/java/dev/cel/checker/CelCheckerLegacyImplTest.java
+++ checker/src/test/java/dev/cel/checker/CelCheckerLegacyImplTest.java
@@ -17,12 +17,15 @@ package dev.cel.checker;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 
+import dev.cel.expr.Type;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 import com.google.protobuf.Duration;
 import com.google.protobuf.FieldMask;
 import com.google.testing.junit.testparameterinjector.TestParameter;
 import com.google.testing.junit.testparameterinjector.TestParameterInjector;
+import dev.cel.checker.CelCheckerLegacyImpl.LegacyBridgeCombinedTypeProvider;
 import dev.cel.checker.CelStandardDeclarations.StandardFunction;
 import dev.cel.common.CelAbstractSyntaxTree;
 import dev.cel.common.CelContainer;
@@ -33,12 +36,15 @@ import dev.cel.common.CelSource;
 import dev.cel.common.CelValidationException;
 import dev.cel.common.CelVarDecl;
 import dev.cel.common.ast.CelExpr;
+import dev.cel.common.types.CelKind;
 import dev.cel.common.types.CelType;
 import dev.cel.common.types.CelTypeProvider;
 import dev.cel.common.types.EnumType;
 import dev.cel.common.types.ListType;
 import dev.cel.common.types.MapType;
+import dev.cel.common.types.ProtoMessageType;
 import dev.cel.common.types.SimpleType;
+import dev.cel.common.types.StructType;
 import dev.cel.common.types.StructTypeReference;
 import dev.cel.common.types.TypeType;
 import dev.cel.compiler.CelCompiler;
@@ -324,6 +330,75 @@ public class CelCheckerLegacyImplTest {
   }
 
   @Test
+  public void check_fieldSelection_withLegacyTypeProvider_success() throws Exception {
+    TypeProvider legacyTypeProvider =
+        new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor()));
+    CelCompiler celCompiler =
+        CelCompilerFactory.standardCelCompilerBuilder()
+            .setTypeProvider(legacyTypeProvider)
+            .addVar("msg", StructTypeReference.create("cel.expr.conformance.proto3.TestAllTypes"))
+            .build();
+
+    CelAbstractSyntaxTree ast = celCompiler.compile("msg.single_int64").getAst();
+
+    assertThat(ast.getResultType()).isEqualTo(SimpleType.INT);
+  }
+
+  @Test
+  public void check_undefinedFieldSelection_withLegacyTypeProvider_throws() {
+    TypeProvider legacyTypeProvider =
+        new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor()));
+    CelCompiler celCompiler =
+        CelCompilerFactory.standardCelCompilerBuilder()
+            .setTypeProvider(legacyTypeProvider)
+            .addVar("msg", StructTypeReference.create("cel.expr.conformance.proto3.TestAllTypes"))
+            .build();
+
+    CelValidationException e =
+        assertThrows(
+            CelValidationException.class, () -> celCompiler.compile("msg.undefined").getAst());
+
+    assertThat(e).hasMessageThat().contains("undefined field 'undefined'");
+  }
+
+  @Test
+  public void check_messageCreation_withLegacyTypeProvider_success() throws Exception {
+    TypeProvider legacyTypeProvider =
+        new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor()));
+    CelCompiler celCompiler =
+        CelCompilerFactory.standardCelCompilerBuilder().setTypeProvider(legacyTypeProvider).build();
+
+    CelType resultType =
+        celCompiler
+            .compile("cel.expr.conformance.proto3.TestAllTypes{single_int64: 2}")
+            .getAst()
+            .getResultType();
+
+    // Struct creation resolves to the type handed back by the type provider, which inherits
+    // identity equality, so the assertion is on kind and name rather than on an equal instance.
+    assertThat(resultType.kind()).isEqualTo(CelKind.STRUCT);
+    assertThat(resultType.name()).isEqualTo("cel.expr.conformance.proto3.TestAllTypes");
+  }
+
+  @Test
+  public void lookupEnumValue_legacyTypeProvider_success(
+      @TestParameter({
+            "cel.expr.conformance.proto3.TestAllTypes.NestedEnum.BAZ == 2",
+            ".cel.expr.conformance.proto3.TestAllTypes.NestedEnum.BAZ == 2"
+          })
+          String expr)
+      throws Exception {
+    TypeProvider legacyTypeProvider =
+        new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor()));
+    CelCompiler celCompiler =
+        CelCompilerFactory.standardCelCompilerBuilder().setTypeProvider(legacyTypeProvider).build();
+
+    CelAbstractSyntaxTree ast = celCompiler.compile(expr).getAst();
+
+    assertThat(ast.getResultType()).isEqualTo(SimpleType.BOOL);
+  }
+
+  @Test
   public void lookupEnumValue_modernTypeProvider_success(
       @TestParameter({
             "cel.expr.conformance.proto3.TestAllTypes.NestedEnum.BAZ == 2",
@@ -392,6 +467,359 @@ public class CelCheckerLegacyImplTest {
     CelAbstractSyntaxTree ast = celCompiler.compile("custom.MyType").getAst();
 
     assertThat(ast.getResultType()).isEqualTo(preWrappedType);
+  }
+
+  @Test
+  public void check_legacyTypeProviderDeclaringNonStructType_success() throws Exception {
+    TypeProvider customLegacyProvider =
+        new TypeProvider() {
+          @Override
+          public Type lookupType(String typeName) {
+            if (typeName.equals("user.org_units")) {
+              return Type.newBuilder()
+                  .setListType(
+                      Type.ListType.newBuilder()
+                          .setElemType(Type.newBuilder().setPrimitive(Type.PrimitiveType.INT64)))
+                  .build();
+            }
+            return null;
+          }
+
+          @Override
+          public Integer lookupEnumValue(String enumName) {
+            return null;
+          }
+
+          @Override
+          public FieldType lookupFieldType(Type type, String fieldName) {
+            return null;
+          }
+        };
+    CelCompiler celCompiler =
+        CelCompilerFactory.standardCelCompilerBuilder()
+            .setTypeProvider(customLegacyProvider)
+            .build();
+
+    CelAbstractSyntaxTree ast = celCompiler.compile("size(user.org_units) == 0").getAst();
+
+    assertThat(ast.getResultType()).isEqualTo(SimpleType.BOOL);
+  }
+
+  private static CelCompiler newCompilerWithUnenumerableJsonNameMessage() {
+    ProtoMessageType modernUnenumerable =
+        ProtoMessageType.createWithUnenumerableFields(
+            "custom.UnenumerableMessage",
+            fieldName ->
+                fieldName.equals("myField") || fieldName.equals("my_field")
+                    ? Optional.of(SimpleType.INT)
+                    : Optional.empty(),
+            extensionName -> Optional.empty(),
+            "myField"::equals);
+    CelTypeProvider modernProvider =
+        new CelTypeProvider() {
+          @Override
+          public ImmutableList<CelType> types() {
+            return ImmutableList.of();
+          }
+
+          @Override
+          public Optional<CelType> findType(String typeName) {
+            return typeName.equals("custom.UnenumerableMessage")
+                ? Optional.of(modernUnenumerable)
+                : Optional.empty();
+          }
+        };
+    TypeProvider legacyTypeProvider =
+        new TypeProvider() {
+          @Override
+          public Type lookupType(String typeName) {
+            return null;
+          }
+
+          @Override
+          public Integer lookupEnumValue(String enumName) {
+            return null;
+          }
+
+          @Override
+          public FieldType lookupFieldType(Type type, String fieldName) {
+            return null;
+          }
+        };
+    return CelCompilerFactory.standardCelCompilerBuilder()
+        .setTypeProvider(modernProvider)
+        .setTypeProvider(legacyTypeProvider)
+        .addVar("msg", StructTypeReference.create("custom.UnenumerableMessage"))
+        .build();
+  }
+
+  @Test
+  public void check_unenumerableMessageType_withJsonName_addsJsonNameExtension() throws Exception {
+    CelCompiler celCompiler = newCompilerWithUnenumerableJsonNameMessage();
+
+    CelAbstractSyntaxTree ast = celCompiler.compile("msg.myField == 1").getAst();
+
+    assertThat(ast.getResultType()).isEqualTo(SimpleType.BOOL);
+    assertThat(ast.getSource().getExtensions())
+        .contains(
+            CelSource.Extension.create(
+                "json_name",
+                CelSource.Extension.Version.of(1, 1),
+                CelSource.Extension.Component.COMPONENT_RUNTIME));
+  }
+
+  @Test
+  public void check_unenumerableMessageType_withoutJsonName_doesNotAddExtension() throws Exception {
+    CelCompiler celCompiler = newCompilerWithUnenumerableJsonNameMessage();
+
+    CelAbstractSyntaxTree nonJsonAst = celCompiler.compile("msg.my_field == 1").getAst();
+
+    assertThat(nonJsonAst.getResultType()).isEqualTo(SimpleType.BOOL);
+    assertThat(nonJsonAst.getSource().getExtensions()).isEmpty();
+  }
+
+  @Test
+  public void check_extensionField_withUnenumerableModernMessageAndLegacyExtensionProvider_success()
+      throws Exception {
+    ProtoMessageType unenumerableMessage =
+        ProtoMessageType.createWithUnenumerableFields(
+            "cel.expr.conformance.proto2.TestAllTypes",
+            fieldName -> Optional.empty(),
+            extensionName -> Optional.empty(),
+            unused -> false);
+    CelTypeProvider modernProvider =
+        new CelTypeProvider() {
+          @Override
+          public ImmutableList<CelType> types() {
+            return ImmutableList.of();
+          }
+
+          @Override
+          public Optional<CelType> findType(String typeName) {
+            return typeName.equals("cel.expr.conformance.proto2.TestAllTypes")
+                ? Optional.of(unenumerableMessage)
+                : Optional.empty();
+          }
+        };
+    TypeProvider legacyExtensionProvider =
+        new DescriptorTypeProvider(
+            ImmutableList.of(
+                TestAllTypesProto.getDescriptor(), TestAllTypesExtensions.getDescriptor()));
+    CelChecker celChecker =
+        CelCompilerFactory.standardCelCheckerBuilder()
+            .setTypeProvider(modernProvider)
+            .setTypeProvider(legacyExtensionProvider)
+            .addVarDeclarations(
+                CelVarDecl.newVarDeclaration(
+                    "msg", StructTypeReference.create("cel.expr.conformance.proto2.TestAllTypes")))
+            .build();
+    CelAbstractSyntaxTree parsedAst =
+        CelAbstractSyntaxTree.newParsedAst(
+            CelExpr.ofSelect(
+                2L,
+                CelExpr.ofIdent(1L, "msg"),
+                "cel.expr.conformance.proto2.int32_ext",
+                /* isTestOnly= */ false),
+            CelSource.newBuilder().build());
+
+    CelAbstractSyntaxTree checkedAst = celChecker.check(parsedAst).getAst();
+
+    assertThat(checkedAst.getResultType()).isEqualTo(SimpleType.INT);
+  }
+
+  @Test
+  public void combinedTypeProvider_enumerableModernMessage_preservesFieldNames() {
+    ProtoMessageType modernMessage =
+        ProtoMessageType.create(
+            "cel.expr.conformance.proto3.TestAllTypes",
+            ImmutableSet.of("single_int32", "single_int64"),
+            fieldName -> Optional.of(SimpleType.INT),
+            extensionName -> Optional.empty(),
+            /* jsonNameResolver= */ fieldName -> false);
+    CelTypeProvider modernProvider =
+        new CelTypeProvider() {
+          @Override
+          public ImmutableList<CelType> types() {
+            return ImmutableList.of();
+          }
+
+          @Override
+          public Optional<CelType> findType(String typeName) {
+            return typeName.equals("cel.expr.conformance.proto3.TestAllTypes")
+                ? Optional.of(modernMessage)
+                : Optional.empty();
+          }
+        };
+    LegacyTypeProviderBridge legacyBridge =
+        new LegacyTypeProviderBridge(
+            new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor())));
+    LegacyBridgeCombinedTypeProvider combinedProvider =
+        new LegacyBridgeCombinedTypeProvider(modernProvider, legacyBridge);
+
+    Optional<CelType> resolvedType =
+        combinedProvider.findType("cel.expr.conformance.proto3.TestAllTypes");
+
+    assertThat(
+            resolvedType
+                .filter(t -> t instanceof ProtoMessageType)
+                .map(t -> ((ProtoMessageType) t).fieldNames()))
+        .hasValue(ImmutableSet.of("single_int32", "single_int64"));
+    assertThat(
+            resolvedType
+                .filter(t -> t instanceof ProtoMessageType)
+                .flatMap(t -> ((ProtoMessageType) t).findField("single_int32"))
+                .map(StructType.Field::type))
+        .hasValue(SimpleType.INT);
+  }
+
+  @Test
+  public void combinedTypeProvider_unenumerableModernMessage_fieldNamesThrows() {
+    ProtoMessageType modernMessage =
+        ProtoMessageType.createWithUnenumerableFields(
+            "cel.expr.conformance.proto3.TestAllTypes",
+            fieldName -> Optional.empty(),
+            extensionName -> Optional.empty(),
+            fieldName -> false);
+    CelTypeProvider modernProvider =
+        new CelTypeProvider() {
+          @Override
+          public ImmutableList<CelType> types() {
+            return ImmutableList.of();
+          }
+
+          @Override
+          public Optional<CelType> findType(String typeName) {
+            return typeName.equals("cel.expr.conformance.proto3.TestAllTypes")
+                ? Optional.of(modernMessage)
+                : Optional.empty();
+          }
+        };
+    LegacyTypeProviderBridge legacyBridge =
+        new LegacyTypeProviderBridge(
+            new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor())));
+    LegacyBridgeCombinedTypeProvider combinedProvider =
+        new LegacyBridgeCombinedTypeProvider(modernProvider, legacyBridge);
+
+    Optional<CelType> resolvedType =
+        combinedProvider.findType("cel.expr.conformance.proto3.TestAllTypes");
+
+    assertThat(resolvedType.map(CelType::kind)).hasValue(CelKind.STRUCT);
+    ProtoMessageType protoMessageType = (ProtoMessageType) resolvedType.get();
+    IllegalStateException thrown =
+        assertThrows(IllegalStateException.class, protoMessageType::fieldNames);
+    assertThat(thrown).hasMessageThat().contains("cannot be enumerated");
+    assertThrows(IllegalStateException.class, protoMessageType::fields);
+  }
+
+  @Test
+  public void combinedTypeProvider_unenumerableModernMessage_delegatesJsonNameResolverToModern() {
+    ProtoMessageType modernMessage =
+        ProtoMessageType.createWithUnenumerableFields(
+            "cel.expr.conformance.proto3.TestAllTypes",
+            fieldName ->
+                fieldName.equals("modernField") ? Optional.of(SimpleType.STRING) : Optional.empty(),
+            extensionName -> Optional.empty(),
+            "modernJsonField"::equals);
+    CelTypeProvider modernProvider =
+        new CelTypeProvider() {
+          @Override
+          public ImmutableList<CelType> types() {
+            return ImmutableList.of();
+          }
+
+          @Override
+          public Optional<CelType> findType(String typeName) {
+            return typeName.equals("cel.expr.conformance.proto3.TestAllTypes")
+                ? Optional.of(modernMessage)
+                : Optional.empty();
+          }
+        };
+    LegacyTypeProviderBridge legacyBridge =
+        new LegacyTypeProviderBridge(
+            new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor())));
+    LegacyBridgeCombinedTypeProvider combinedProvider =
+        new LegacyBridgeCombinedTypeProvider(modernProvider, legacyBridge);
+
+    Optional<CelType> resolvedType =
+        combinedProvider.findType("cel.expr.conformance.proto3.TestAllTypes");
+
+    assertThat(
+            resolvedType
+                .filter(t -> t instanceof ProtoMessageType)
+                .flatMap(t -> ((ProtoMessageType) t).findField("modernField"))
+                .map(StructType.Field::type))
+        .hasValue(SimpleType.STRING);
+    assertThat(
+            resolvedType
+                .filter(t -> t instanceof ProtoMessageType)
+                .map(t -> ((ProtoMessageType) t).isJsonName("modernJsonField")))
+        .hasValue(true);
+    assertThat(
+            resolvedType
+                .filter(t -> t instanceof ProtoMessageType)
+                .map(t -> ((ProtoMessageType) t).isJsonName("nonExistent")))
+        .hasValue(false);
+  }
+
+  @Test
+  public void combinedTypeProvider_modernExtensionTakesPrecedenceOverLegacy() {
+    ProtoMessageType modernMessage =
+        ProtoMessageType.createWithUnenumerableFields(
+            "cel.expr.conformance.proto2.TestAllTypes",
+            fieldName -> Optional.empty(),
+            extensionName ->
+                extensionName.equals("cel.expr.conformance.proto2.int32_ext")
+                    ? Optional.of(SimpleType.STRING)
+                    : Optional.empty(),
+            fieldName -> false);
+    CelTypeProvider modernProvider =
+        new CelTypeProvider() {
+          @Override
+          public ImmutableList<CelType> types() {
+            return ImmutableList.of();
+          }
+
+          @Override
+          public Optional<CelType> findType(String typeName) {
+            return typeName.equals("cel.expr.conformance.proto2.TestAllTypes")
+                ? Optional.of(modernMessage)
+                : Optional.empty();
+          }
+        };
+    LegacyTypeProviderBridge legacyBridge =
+        new LegacyTypeProviderBridge(
+            new DescriptorTypeProvider(
+                ImmutableList.of(
+                    TestAllTypesProto.getDescriptor(), TestAllTypesExtensions.getDescriptor())));
+    LegacyBridgeCombinedTypeProvider combinedProvider =
+        new LegacyBridgeCombinedTypeProvider(modernProvider, legacyBridge);
+
+    Optional<CelType> resolvedType =
+        combinedProvider.findType("cel.expr.conformance.proto2.TestAllTypes");
+
+    assertThat(
+            resolvedType
+                .filter(t -> t instanceof ProtoMessageType)
+                .map(t -> (ProtoMessageType) t)
+                .flatMap(t -> t.findExtension("cel.expr.conformance.proto2.int32_ext"))
+                .map(ProtoMessageType.Extension::type))
+        .hasValue(SimpleType.STRING);
+  }
+
+  @Test
+  public void check_regularField_withModernMessageAndLegacyTypeProvider_success() throws Exception {
+    TypeProvider legacyTypeProvider =
+        new DescriptorTypeProvider(ImmutableList.of(TestAllTypes.getDescriptor()));
+    CelCompiler celCompiler =
+        CelCompilerFactory.standardCelCompilerBuilder()
+            .addMessageTypes(TestAllTypes.getDescriptor())
+            .setTypeProvider(legacyTypeProvider)
+            .addVar("msg", StructTypeReference.create("cel.expr.conformance.proto3.TestAllTypes"))
+            .build();
+
+    CelAbstractSyntaxTree ast = celCompiler.compile("msg.single_int32 == 1").getAst();
+
+    assertThat(ast.getResultType()).isEqualTo(SimpleType.BOOL);
   }
 
   private enum FieldTypeTestCase {
